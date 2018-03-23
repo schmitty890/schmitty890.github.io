9143983d6bb4023ea85675dc71e819b9387b01a0
Jason Schmitt <schmitty890@gmail.com>
2018-03-23 15:00:03 -0400

    adding giphy

--- assets/js/app-giphy.js
+++ assets/js/app-giphy.js
@@ -1,4 +1,7 @@
 var GiphyStuff = (function() {
+
+    var localStorageRatings = []; // arrays for local storage use
+    var localStorageGiphys = [];
     /**
      * [addButtons buttons to add by default to the page]
      */
@@ -11,23 +14,25 @@ var GiphyStuff = (function() {
     /**
      * [getData get the data from the giphy api]
      */
-    var getData = function(search) {
+    var getData = function(search, offset) {
+        var offset = offset || 0;
         var apiKey = "h31f12EISLE8YkJ1XsRX5Yj8XZxNIgu4";
-        var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${search}&limit=10`;
-
+        var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${search}&limit=10&offset=${offset}`;
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
-            console.log(response);
             printData(response);
-
         });
     };
     /**
      * [printData prints the gifs to the page]
      */
     var printData = function(response) {
+      var url = window.location.href;
+      var anyOffset = getQueryString('offset', url);
+
+      if(anyOffset === null) {
         $("#the-gifs").empty();
         if (response.data.length === 0) {
             $("#the-gifs").prepend(`
@@ -36,19 +41,27 @@ var GiphyStuff = (function() {
             </div>
           `);
         } else {
-            response.data.forEach(function(giphy) {
-                var rating = giphy.rating;
-                var theGiphy = giphy.images.original.url;
-                var theGiphyStill = giphy.images.fixed_width_still.url;
-                $("#the-gifs").prepend(`
-                <div class="col-md-4">
-                  <p class="text-center">Rating: ${rating}  <span class="giphy-unliked"><i class="far fa-heart"></i></span></p>
-                  <img data-state="still" data-still="${theGiphyStill}" data-animate="${theGiphy}" src="${theGiphyStill}">
-                </div>
-              `);
-            });
+          postNewData(response);
         }
-
+      }else {
+        postNewData(response);
+      }
+    };
+    /**
+     * [postNewData post new gifs to the page]
+     */
+    var postNewData = function(response) {
+      response.data.forEach(function(giphy) {
+          var rating = giphy.rating;
+          var theGiphy = giphy.images.original.url;
+          var theGiphyStill = giphy.images.fixed_width_still.url;
+          $("#the-gifs").prepend(`
+          <div class="col-md-4">
+            <p class="text-center">Rating: ${rating}  <span class="giphy-unliked"><i class="far fa-heart"></i></span></p>
+            <img data-state="still" data-still="${theGiphyStill}" data-animate="${theGiphy}" src="${theGiphyStill}">
+          </div>
+        `);
+      });
     };
     /**
      * [initSlick initializes the slick.js carousel]
@@ -68,6 +81,10 @@ var GiphyStuff = (function() {
         //when buttons are clicked
         $(document.body).on('click', '#animal-buttons button', function() {
             var value = $(this).text().trim();
+            if (history.pushState) {
+              var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?search=' + value;
+              window.history.pushState({path:newurl},'',newurl);
+            }
             getData(value);
         });
 
@@ -82,7 +99,6 @@ var GiphyStuff = (function() {
         //when the gif image is clicked, change the state
         $(document.body).on('click', '#the-gifs div img', function() {
             var state = $(this).attr('data-state');
-            console.log(state);
             if (state === 'still') {
                 var animate = $(this).attr('data-animate');
                 $(this).attr('src', animate);
@@ -102,14 +118,70 @@ var GiphyStuff = (function() {
             var rating = $(this).parents('p').text().trim();
             var theGiphy = $(this).parents().siblings('img').attr('data-animate');
             $('.slick-carousel').slick('slickAdd', `<div class="slick-giphy"><img data-animate="${theGiphy}" src="${theGiphy}"/><p>${rating}</p></div>`);
+            addToLocalStorage(rating, theGiphy);
         });
 
         //when user clicks add more gifs
-        $(document.body).on('click', '#addMore', function(event) {
+        $(document.body).on('click', '#addGiphs', function(event) {
             event.preventDefault();
-            console.log('add more');
+
+            var url = window.location.href,
+                offset = getQueryString('offset', url),
+                search = getQueryString('search', url),
+                separator = (url.indexOf("?") === -1) ? "?" : "&";
+            if(offset === null) {
+              offset = 10;
+              if (history.pushState) {
+                var newParam = separator + "offset=" + offset;
+                var newurl = url.replace(newParam, "");
+                    newurl += newParam;
+                window.history.pushState({path:newurl},'',newurl);
+              }
+            }else {
+              offset = Number(offset) + 10;
+              if (history.pushState) {
+                var newurl = url.split( '&' )[0];
+                var newParam = separator + "offset=" + offset;
+                    newurl += newParam;
+                window.history.pushState({path:newurl},'',newurl);
+              }
+            }
+            getData(search, offset);
         });
 
+    };
+    /**
+     * [getQueryString get a specific query parameter in the url]
+     */
+    var getQueryString = function ( field, url ) {
+      var href = url ? url : window.location.href;
+      var reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i' );
+      var string = reg.exec(href);
+      return string ? string[1] : null;
+    };
+    /**
+     * [getLocalStorage get local storage if there is local storage.
+     * If there is local storage, populate the slick.js carousel with what is in local storage.]
+     */
+    var getLocalStorage = function() {
+      var ratings = localStorage.getItem("ratings");
+      var giphys = localStorage.getItem("giphys");
+      if(typeof ratings === 'string') {
+        localStorageGiphys = giphys = giphys.split(',');
+        localStorageRatings = ratings = ratings.split(',');
+        giphys.forEach(function(element, index) {
+          $('.slick-carousel').slick('slickAdd', `<div class="slick-giphy"><img data-animate="${element}" src="${element}"/><p>${ratings[index]}</p></div>`);
+        });        
+      }
+    };
+    /**
+     * [addToLocalStorage add items to local storage when the like (heart) button is clicked]
+     */
+    var addToLocalStorage = function(rating, theGiphy) {
+      localStorageRatings.push(rating);
+      localStorageGiphys.push(theGiphy);
+      localStorage.setItem("ratings", localStorageRatings);
+      localStorage.setItem("giphys", localStorageGiphys);
     };
     /**
      * [init functions]
@@ -118,6 +190,13 @@ var GiphyStuff = (function() {
         addButtons();
         initSlick();
         eventHandlers();
+        //make sure slickjs carousel is on the page before checking local storage
+        var checkExist = setInterval(function() {
+           if ($('.slick-initialized').length) {
+              getLocalStorage();
+              clearInterval(checkExist);
+           }
+        }, 100);
     };
 
     return {
