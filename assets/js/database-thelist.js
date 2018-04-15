635dae0aa3efe2f937a7e5ba8a35551134a3649f
Jason Schmitt <schmitty890@gmail.com>
2018-04-15 15:17:16 -0400

    adding the list authentication

---
+++ assets/js/database-thelist.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBxA56nt_KQ0R4KRs_UNlfIVRrS_CEGP0I",
+  authDomain: "grocery-2efc5.firebaseapp.com",
+  databaseURL: "https://grocery-2efc5.firebaseio.com",
+  projectId: "grocery-2efc5",
+  storageBucket: "",
+  messagingSenderId: "588484982076"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
